d292ef213aa94c3da3b940e69cf4b52b4013e67f
Lucas Gabriel Pereira da Silva <llucasgabriel@hotmail.com>
2024-01-29 16:46:54 -0300

    Adicionado subscribe dinâmico do RabbitMQ

--- src/ProductApi/Products.WebApi/Configurations/MessageBrokerConfig.cs
+++ src/ProductApi/Products.WebApi/Configurations/MessageBrokerConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Products.Application.Categories.Events;
 using Products.Application.Configuration;
 using Products.Application.Products.Events;
+using System.Reflection;
 
 namespace Products.WebApi.Configurations
 {
@@ -8,26 +9,42 @@ namespace Products.WebApi.Configurations
     {
         public static void AddMessageBrokerConfig(this WebApplication app)
         {
-            using (var scope = app.Services.CreateScope())
+            var currentAssembly = typeof(IMediatorHandler).Assembly;
+            var eventHandlersTypes = currentAssembly.GetTypes().Where(t =>
+                t.FullName.StartsWith("Products.Application") &&
+                t.FullName.EndsWith("EventHandler"));
+
+            foreach (var eventHandlerType in eventHandlersTypes)
             {
-                var rabbitEventBus = scope.ServiceProvider.GetRequiredService<IMediatorHandler>();
-                AddScopedServiceCategory(rabbitEventBus);
-                AddScopedServiceProduct(rabbitEventBus);
+                string domain = eventHandlerType.FullName.Split('.')[2];
+                if (domain != null)
+                {
+                    // Find event types in the same namespace as the event handler
+                    var eventTypes = currentAssembly.GetTypes().Where(t =>
+                        t.FullName.StartsWith($"Products.Application.{domain}") &&
+                        t.FullName.EndsWith("Event"));
+
+                    using (var scope = app.Services.CreateScope())
+                    {
+                        var serviceProvider = scope.ServiceProvider;
+
+                        // Get the IMediatorHandler service
+                        var mediatorHandler = serviceProvider.GetRequiredService<IMediatorHandler>();
+
+                        // Subscribe each event type to the corresponding event handler type
+                        foreach (var eventType in eventTypes)
+                        {
+                            // Use reflection to call Subscribe method with generic arguments
+                            var method = typeof(IMediatorHandler).GetMethod("Subscribe");
+                            var genericMethod = method.MakeGenericMethod(eventType, eventHandlerType);
+                            genericMethod.Invoke(mediatorHandler, null);
+                        }
+                    }
+                }
+
             }
-        }
 
-        private static void AddScopedServiceCategory(IMediatorHandler rabbitEventBus)
-        {
-            rabbitEventBus.Subscribe<CategoryRegisterEvent, CategoryEventHandler>();
-            rabbitEventBus.Subscribe<CategoryUpdateEvent, CategoryEventHandler>();
-            rabbitEventBus.Subscribe<CategoryDeleteEvent, CategoryEventHandler>();
-        }
 
-        private static void AddScopedServiceProduct(IMediatorHandler rabbitEventBus)
-        {
-            rabbitEventBus.Subscribe<ProductRegisterEvent, ProductEventHandler>();
-            rabbitEventBus.Subscribe<ProductUpdateEvent, ProductEventHandler>();
-            rabbitEventBus.Subscribe<ProductDeleteEvent, ProductEventHandler>();
         }
     }
 }
